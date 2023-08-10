12ac1c1d363badba5f9ceec4102f2dfb50e683b5
wttwhite <wangtt12034@126.com>
2023-08-10 22:34:15 +0800

    1

--- src/page/menuPage/mock.js
+++ src/page/menuPage/mock.js
@@ -1,17 +1,19 @@
 export const MockData = [
   {
     id: 1,
-    name: '我是名称1',
+    name: '白菜虾仁豆腐汤',
     type: 1,
     num: 0,
     summary: 1,
     remark: '',
     pic: 'https://img2.baidu.com/it/u=3853345508,384760633&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=1200',
-    material: '111、2222、3333、444444、55555、34234234、342553、535454',
+    material: '娃娃菜、虾仁、嫩豆腐、鸡蛋、香葱、蒜瓣',
     step: `
-    1. aaaaaaaaaaaaaaaaaaa,
-    2. bbbbbbbbbbbbb
-    3. cccccccccc
+    1. 鸡蛋煎熟，用锅铲切块盛出备用,
+    2. 锅内加少许油，下虾仁煎至断生，加入葱蒜末爆香，把前面的鸡蛋加进来
+    3. 倒入适量的开水，再下豆腐块，再加入大白菜
+    4. 关上盖子焖煮至白菜透明，加一点盐调味
+    5. 出锅前撒点枸杞，汤水超清甜
     `,
     time: '30',
   },
